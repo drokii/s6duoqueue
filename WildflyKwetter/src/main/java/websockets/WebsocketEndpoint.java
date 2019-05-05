9d739f51893a37fd65d4dee3c5eb354ee6cc50ef
Desktop Computer - Pedro <pedrodossantosmarques@gmail.com>
2019-05-05 15:52:51 +0200

    websockets fully working :cool:

--- WildflyKwetter/src/main/java/websockets/WebsocketEndpoint.java
+++ WildflyKwetter/src/main/java/websockets/WebsocketEndpoint.java
@@ -1,46 +1,47 @@
 package websockets;
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.websocket.*;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 
-@ApplicationScoped
+
 @ServerEndpoint("/kwettersocket")
 public class WebsocketEndpoint {
 
-    HashSet<Session> sessions;
-
-    @PostConstruct
-    public void init(){
-        sessions = new HashSet<>();
-    }
+    private static Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
 
     @OnOpen
     public void onOpen(Session session) {
+        sessions.add(session);
         System.out.println("A client connected!");
-         sessions.add(session);
+        System.out.println("Sessions connected: " + sessions.size());
+
     }
 
     @OnMessage
-    public void onMessage(String msg) {
+    public void onMessage(String msg) throws IOException {
 
         System.out.println(msg);
-        try {
+        synchronized (sessions) {
+
             for (Session s : sessions
             ) {
                 s.getBasicRemote().sendText("Refresh, Please.");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
         }
+
 
     }
 
     @OnClose
-    public void onClose(Session session){
+    public void onClose(Session session) {
         sessions.remove(session);
     }
 
